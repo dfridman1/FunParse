dfe8fe4c99d14b5b2dd96f36dcfc1af02970f938
Denys Fridman <denys.fridman@gmail.com>
2020-10-24 14:08:23 +0200

    first commit

---
+++ src/Parsec.hs
@@ -0,0 +1,91 @@
+module Parsec where
+
+
+import Control.Applicative hiding (optional)
+import Control.Monad
+import Data.Functor
+
+
+data ErrorMsg = EOF
+              | MatchFail deriving Show
+
+
+data Parser a = Parser { runParser :: String -> Either ErrorMsg (String, a) }
+
+
+instance Functor Parser where
+    f `fmap` pr = pr >>= return . f
+
+
+instance Applicative Parser where
+    pure = return
+    f <*> pr = f >>= \g -> g `fmap` pr
+
+
+instance Alternative Parser where
+    empty = Parser $ \_ -> Left EOF
+
+    lPr <|> rPr = Parser $ \s -> case (runParser lPr s, runParser rPr s) of
+                                    (t@(Right _), _) -> t 
+                                    (_, t@(Right _)) -> t
+                                    (t, _)          -> t
+
+
+oneOf :: (Foldable t, Alternative f) => t (f a) -> f a
+oneOf = foldl (<|>) empty 
+
+
+instance Monad Parser where
+    return x = Parser $ \s -> Right (s, x)
+    
+    pr >>= f = Parser $ \s -> case runParser pr s of
+                                Left err      -> Left err
+                                Right (s', r) -> runParser (f r) s'
+
+
+char :: Char -> Parser Char
+char ch = Parser $ \s -> case s of
+                             (x: xs) -> if x == ch then Right (xs, ch) else Left MatchFail
+                             []      -> Left EOF
+
+
+string :: String -> Parser String
+string [] = return []
+string (x: xs) = do
+    y  <- char x
+    y' <- string xs
+    return (y: y')
+
+
+notChar :: Char -> Parser Char
+notChar ch = Parser $ \s -> case s of
+                                (x: xs) -> if x /= ch then Right (xs, x) else Left MatchFail
+                                []      -> Left EOF 
+
+
+anyChar :: Parser Char
+anyChar = Parser $ \s -> case s of
+                            (x: xs) -> Right (xs, x)
+                            []      -> Left EOF
+
+
+emptyP :: Parser ()
+emptyP = return ()
+
+
+eof :: Parser ()
+eof = Parser $ \s -> case s of
+                        []        -> Right ([], ())
+                        otherwise -> Left MatchFail
+
+
+ignoreP :: Parser a -> Parser ()
+ignoreP pr = pr >> return () 
+
+
+option :: a -> Parser a -> Parser a
+option x pr = pr <|> (return x)
+
+
+optional :: Parser a -> Parser ()
+optional pr = (ignoreP pr) <|> emptyP
